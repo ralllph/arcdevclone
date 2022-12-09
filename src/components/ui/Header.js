822483d0557acbd45c209f1a93c7aaf8a9216ad5
Raphael Izueke <raphael.izueke@flutterwavego.com>
2022-12-09 14:17:21 +0100

    Project setup

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import AppBar from "@material-ui/core/AppBar"
+import { Toolbar } from "@material-ui/core"
+
+const Header = (props)=>{
+    return(
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default Header
